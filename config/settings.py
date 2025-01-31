18c4337fd3de468dabe650cb72209c9277324b94
elomek <elhamkramian9@gmail.comgit config --global user.email elhamkaramian90@gmail.comgit config --global user.email elhamkramian9@gmail.com>
2025-01-31 10:15:15 +0100

    create db postgresql and add to settings.py

--- config/settings.py
+++ config/settings.py
@@ -81,14 +81,25 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('Review'), 
+        'USER': os.getenv('Elom'), 
+        'PASSWORD':os.getenv('Elham3950392238!'),  
+        'HOST': 'localhost',  
+        'PORT': '5432',
+    },
+
+    'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
